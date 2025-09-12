7272a610457e52c426e4c142ca7276eb5066eb02
lmz <201422211+lmz-c@users.noreply.github.com>
2025-09-12 20:04:38 +0800

    pages update -4

--- packages/docs/.vitepress/demo/alert/Desc.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div style="max-width: 600px">
-    <er-alert
-      title="With description"
-      type="success"
-      description="This is a description."
-    />
-  </div>
-</template>
--- packages/docs/.vitepress/demo/alert/ShowIcon.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div style="max-width: 600px">
-    <er-alert title="Success alert" type="success" show-icon />
-    <er-alert title="Info alert" type="info" show-icon />
-    <er-alert title="Warning alert" type="warning" show-icon />
-    <er-alert title="Error alert" type="danger" show-icon />
-  </div>
-</template>
